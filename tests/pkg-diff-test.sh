1e88239932955975e075b46a9bb1f72d5aa95271
Bernhard M. Wiedemann <bwiedemann@suse.de>
2018-07-21 05:29:28 +0200

    tests: check for diff output

--- tests/pkg-diff-test.sh
+++ tests/pkg-diff-test.sh
@@ -14,10 +14,26 @@ it_finds_text_diff()
     ! $p rpms/stringtext-1-[02].*.rpm
 }
 
+it_prints_md5_diff()
+{
+    $p rpms/stringtext-1-[02].*.rpm | grep '^-/usr/share/doc/packages/stringtext/string.txt f447b20a7fcbf53a5d5be013ea0b1' #= echo 123456|md5sum
+}
+
+it_prints_text_diff()
+{
+    $p rpms/stringtext-1-[02].*.rpm | grep '^-123456$'
+    $p rpms/stringtext-1-[02].*.rpm | grep '^+++ new//usr/share/doc/packages/stringtext/string.txt'
+}
+
 it_finds_diff_even_with_identical_files()
 {
     ! $p -a rpms/stringtext-1-1[01].*.rpm
     ! $p -a rpms/stringtext-1-1[02].*.rpm
     ! $p -a rpms/stringtext-1-1[12].*.rpm
+}
+
+it_reports_missing_files()
+{
     ! $p -a rpms/stringtext-1-{2,12}.*.rpm
+    $p -a rpms/stringtext-1-{2,12}.*.rpm | grep 'string2.txt differs'
 }
