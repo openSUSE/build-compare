openSUSE/build-compare

commit 67384203963a87f9e3071016957c25a0f0e5adf1

Fix syntax in same-build-result.sh.
build-compare rev 61 in openSUSE:Factory

same-build-result.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2009, 2010 SUSE Linux Product GmbH, Germany.
+# Copyright (c) 2009, 2010, 2012 SUSE Linux Product GmbH, Germany.
 # Licensed under GPL v2, see COPYING file for details.
 #
 # Written by Adrian Schroeter <adrian@suse.de>
@@ -83,6 +83,18 @@ done
 if [ -n "${NEWRPMS[0]}" ]; then
   echo additional new package
   exit 1
+fi
+
+# Compare rpmlint.log files
+RPMLINTDIR=/home/abuild/rpmbuild/OTHER
+
+if test -e $OLDDIR/rpmlint.log -a -e $RPMLINTDIR/rpmlint.log; then
+  echo "comparing $OLDDIR/rpmlint.log and $RPMLINTDIR/rpmlint.log"
+  if ! cmp -s $OLDDIR/rpmlint.log $RPMLINTDIR/rpmlint.log; then
+    echo "rpmlint.log files differ:"
+    diff -u $OLDDIR/rpmlint.log $RPMLINTDIR/rpmlint.log|head -n 20
+    exit 1
+  fi
 fi
 
 if test $SUCCESS -eq 0; then

build-compare.spec
@@ -1,7 +1,7 @@
 #
 # spec file for package build-compare
 #
-# Copyright (c) 2011 SUSE LINUX Products GmbH, Nuernberg, Germany.
+# Copyright (c) 2012 SUSE LINUX Products GmbH, Nuernberg, Germany.
 #
 # All modifications and additions to the file contributed by third parties
 # remain the property of their copyright owners, unless otherwise agreed
@@ -21,7 +21,7 @@ Name:           build-compare
 License:        GPL-2.0+
 Group:          Development/Tools/Building
 Summary:        Build Result Compare Script
-Version:        2011.04.19
+Version:        2012.01.09
 Release:        6
 Source1:        COPYING
 Source2:        same-build-result.sh

commit f48f0e24a82fd76ad2a6ae0ca97263695a82d70c

Fix result in case no rpmlint.log exist
Commit 8a02e497e5209ae0ee8d394a0d29f3018f9a14e6 revealed a logic error
introduced by 67384203963a87f9e3071016957c25a0f0e5adf1

Avoid republish if no rpmlint.log exists.

Signed-off-by: Olaf Hering <olaf@aepfle.de>

same-build-result.sh
@@ -189,9 +189,17 @@ if test -n "$OTHERDIR"; then
       SUCCESS=0
     fi
     rm $file1 $file2
-  elif test -e ${new_log} ; then
-    echo "rpmlint.log is new"
-    SUCCESS=0
+  else
+    if test -e "${new_log}"
+    then
+      echo "rpmlint.log is new"
+    elif test -e "${old_log}"
+    then
+      echo "rpmlint.log disappeared"
+    else
+      SUCCESS=0
+      echo "No rpmlint.log available"
+    fi
   fi
 
   appdatas=$(cd $OTHERDIR && find . -name "*-appdata.xml")